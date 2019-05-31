5068f29cbfba7e101423b87b8c4c5104c3ff25b0
Luiz Henrique Araujo Mello <luiz-mello@procergs.rs.gov.br>
2019-05-31 14:27:54 -0300

    adicionado namespace

--- app/ts/views/NegociacoesView.ts
+++ app/ts/views/NegociacoesView.ts
@@ -1,33 +1,38 @@
-class NegociacoesView extends View<Negociacoes> {  
+namespace Views {
 
-    template(model: Negociacoes): string {
-        return `
-            <table class="table table-hover table-bordered">
-                <thead>
-                    <tr>
-                        <th>DATA</th>
-                        <th>QUANTIDADE</th>
-                        <th>VALOR</th>
-                        <th>VOLUME</th>
-                    </tr>
-                </thead>
+    import View = Views.View;
 
-                <tbody>
-                    ${model.paraArray().map(negociacao => 
-                        `
+    export class NegociacoesView extends View<Negociacoes> {  
+
+        template(model: Negociacoes): string {
+            return `
+                <table class="table table-hover table-bordered">
+                    <thead>
                         <tr>
-                            <td>${negociacao.data.getDay()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</td>
-                            <td>${negociacao.quantidade}</td>
-                            <td>${negociacao.valor}</td>
-                            <td>${negociacao.volume}</td>
+                            <th>DATA</th>
+                            <th>QUANTIDADE</th>
+                            <th>VALOR</th>
+                            <th>VOLUME</th>
                         </tr>
-                        `
-                    ).join('')}
-                </tbody>
+                    </thead>
 
-                <tfoot>
-                </tfoot>
-            </table>     
-        `;
+                    <tbody>
+                        ${model.paraArray().map(negociacao => 
+                            `
+                            <tr>
+                                <td>${negociacao.data.getDay()}/${negociacao.data.getMonth() + 1}/${negociacao.data.getFullYear()}</td>
+                                <td>${negociacao.quantidade}</td>
+                                <td>${negociacao.valor}</td>
+                                <td>${negociacao.volume}</td>
+                            </tr>
+                            `
+                        ).join('')}
+                    </tbody>
+
+                    <tfoot>
+                    </tfoot>
+                </table>     
+            `;
+        }
     }
 }
